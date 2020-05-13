d0ff340d29d9fc87f309262d63ecff7b8141b184
jourm <jourm91@gmail.com>
2020-05-13 11:08:38 +0000

    Create product detail view that renders diferent templates based on product type

--- products/utils.py
+++ products/utils.py
@@ -3,7 +3,11 @@ from django.shortcuts import get_object_or_404
 
 
 def get_product(product_id):
-    """# Write the logic that retrieve the product type"""
+    """ Helper function for managing polymorphism, id are shared between the parent
+    Product table and children, This function returns the corresponding child record from the parent id.
+    Input: product.id of from Product table
+    Output: List: [table that id was found in,
+     Record from the correct child table] """
     test = Product.objects.get(pk=product_id)
     models = [["yeast", Yeast], ["malt", Malt], ["hop", Hop],
               ["equipment", Hop], ["book", Book]]
--- products/views.py
+++ products/views.py
@@ -19,13 +19,13 @@ def product_detail(request, product_id):
     context = {'product': product[1], }
 
     if product[0] == "hop":
-        return render(request, 'products/hop_detail.html', context)
-    elif product[0] == "malt":
-        return render(request, 'products/malt_detail.html', context)
-    elif product[0] == "yeast":
-        return render(request, 'products/yeast_detail.html', context)
-    elif product[0] == "equipment":
-        return render(request, 'products/equipment_detail.html', context)
-    elif product[0] == "book":
-        return render(request, 'products/book_detail.html', context)
+        return render(request, 'products/hop_details.html', context)
+    #elif product[0] == "malt":
+    #    return render(request, 'products/malt_detail.html', context)
+    #elif product[0] == "yeast":
+    #    return render(request, 'products/yeast_detail.html', context)
+    #elif product[0] == "equipment":
+    #    return render(request, 'products/equipment_detail.html', context)
+    #elif product[0] == "book":
+    #    return render(request, 'products/book_detail.html', context)
 
