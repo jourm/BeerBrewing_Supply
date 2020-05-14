700fd96d2739c3f03fb518585b263ff7b40404ca
jourm <jourm91@gmail.com>
2020-05-14 09:59:05 +0000

    Add automated testing to products app

--- products/tests.py
+++ products/tests.py
@@ -1,4 +1,23 @@
 from django.test import TestCase
+from.models import Book
 
 # Create your tests here.
 
+
+class TestProductViews(TestCase):
+
+    def test_get_products(self):
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    def test_get_product_details(self):
+        book = Book.objects.create(
+            name='beer',
+            price=10,
+            author='me',
+            description='book about beer'
+            )
+        response = self.client.get('/products/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_details.html')
