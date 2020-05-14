b03e9b76742d08ea8b07242ce94ad5d148224f49
jourm <jourm91@gmail.com>
2020-05-14 12:38:28 +0000

    add context processor to cart app

--- BeerBrewing_supply/settings.py
+++ BeerBrewing_supply/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth', # Required by allauth
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_content',
             ],
         },
     },
