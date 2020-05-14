d9b7ecf71a7739cb8ecb74f8e94a493350af5984
jourm <jourm91@gmail.com>
2020-05-14 09:36:41 +0000

    extend product_details to handle all kinds of products

--- products/views.py
+++ products/views.py
@@ -24,14 +24,4 @@ def product_detail(request, product_id):
     product = get_product(product_id)
     context = {'product': product[1], }
 
-    if product[0] == "hop":
-        return render(request, 'products/hop_details.html', context)
-    #elif product[0] == "malt":
-    #    return render(request, 'products/malt_detail.html', context)
-    #elif product[0] == "yeast":
-    #    return render(request, 'products/yeast_detail.html', context)
-    #elif product[0] == "equipment":
-    #    return render(request, 'products/equipment_detail.html', context)
-    #elif product[0] == "book":
-    #    return render(request, 'products/book_detail.html', context)
-
+    return render(request, 'products/product_details.html', context)
