684c8edcef1b33fa3dbce765b0228745eba63522
jourm <jourm91@gmail.com>
2020-05-18 14:46:29 +0000

    create order model

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -7,9 +8,15 @@ def orders(request):
     return render(request, 'orders/orders.html')
 
 
+@csrf_exempt
 def register_order(request):
+    print('RECEIVED REQUEST =)')
+    print(request.method)
+    print('\n*HEADERS')
+    print(request.headers)
+    print('\n*BODY')
+    print(request.body)
     if request.POST:
-        print(request.POST)
+        print(request.POST, request.headers)
 
     return render(request, 'orders/orders.html')
-
