ae2cc0dbc4460ae850a98c1afc626006c3382ee2
jourm <jourm91@gmail.com>
2020-05-18 15:38:18 +0000

    Add Order to site admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+
+
+    readonly_fields = ('order_id', 'date',
+                       'order_total',
+                       'klarna_line_items',)
+
+    fields = ('order_id', 'date', 'given_name', 'family_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'county',
+              'order_total', 'grand_total', 'klarna_line_items')
+
+    list_display = ('order_id', 'date', 'given_name',
+                    'family_name', 'order_total')
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
+
