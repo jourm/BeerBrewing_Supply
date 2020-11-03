c131a7c15e1e7608484e1ff89748a74bd9fad692
jourm <jourm91@gmail.com>
2020-11-03 15:06:57 +0000

    Fix spelling error in blog/urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('add_blog', views.edit_blog, name='add_blog'),
     path('', views.view_blogs, name='view_blogs'),
-    path('edit_blog/<blog_id>', views._blog, name='edit_blog'),
+    path('edit_blog/<blog_id>', views.edit_blog, name='edit_blog'),
     path('delete_blog/<blog_id>', views.delete_blog, name='delete_blog'),
 ]
