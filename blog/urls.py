5c96590baee3bd23050f20cfd29f635dcf837449
jourm <jourm91@gmail.com>
2020-11-05 09:48:59 +0000

    add empty edit blog url

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('add_blog', views.edit_blog, name='add_blog'),
     path('', views.view_blogs, name='view_blogs'),
     path(r'^/edit_blog/(?P<blog_id>\d+)$', views.edit_blog, name='edit_blog'),
+    path('/edit_blog/', views.edit_blog, name='edit_blog'),
     path('delete_blog/<blog_id>', views.delete_blog, name='delete_blog'),
 ]
+
